2d60418549ba56272ffe9b76d9a11ab6ffefad50
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-11-07 00:41:20 +0100

    feat(adapter): add separating hyperplane problem adapter

--- src/utils/problemAdapter.ts
+++ src/utils/problemAdapter.ts
@@ -2,6 +2,8 @@ import { ProblemDefinition } from '../types/experiments';
 import { ProblemFunctions } from '../algorithms/types';
 import { DataPoint } from '../shared-utils';
 import { logisticObjective, logisticGradient, logisticHessian } from './logisticRegression';
+import * as SH from './separatingHyperplane';
+import { SeparatingHyperplaneVariant } from '../types/experiments';
 
 /**
  * Convert a ProblemDefinition from the problem registry into ProblemFunctions format
@@ -28,4 +30,42 @@ export function logisticRegressionToProblemFunctions(
     hessian: (w: number[]) => logisticHessian(w, data, lambda),
     dimensionality: 3, // Logistic regression uses [w0, w1, w2] with bias
   };
+}
+
+/**
+ * Adapter for separating hyperplane problems.
+ * Converts variant-specific functions to ProblemFunctions format.
+ */
+export function separatingHyperplaneToProblemFunctions(
+  data: DataPoint[],
+  variant: SeparatingHyperplaneVariant
+): ProblemFunctions {
+  let objective: (w: number[]) => number;
+  let gradient: (w: number[]) => number[];
+  let hessian: (w: number[]) => number[][];
+
+  switch (variant) {
+    case 'hard-margin':
+      objective = (w) => SH.hardMarginObjective(w, data);
+      gradient = (w) => SH.hardMarginGradient(w, data);
+      hessian = (w) => SH.hardMarginHessian(w, data);
+      break;
+    case 'soft-margin':
+      objective = (w) => SH.softMarginObjective(w, data);
+      gradient = (w) => SH.softMarginGradient(w, data);
+      hessian = (w) => SH.softMarginHessian(w, data);
+      break;
+    case 'perceptron':
+      objective = (w) => SH.perceptronObjective(w, data);
+      gradient = (w) => SH.perceptronGradient(w, data);
+      hessian = (w) => SH.perceptronHessian(w, data);
+      break;
+    case 'squared-hinge':
+      objective = (w) => SH.squaredHingeObjective(w, data);
+      gradient = (w) => SH.squaredHingeGradient(w, data);
+      hessian = (w) => SH.squaredHingeHessian(w, data);
+      break;
+  }
+
+  return { objective, gradient, hessian };
 }
